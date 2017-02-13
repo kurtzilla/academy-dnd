6868daca5d61a760ec59bfc842d3d11caa44293d
rob kurtz <rob@robkurtz.net>
2017-02-13 09:35:16 -0700

    initial commit

---
+++ src/js/components/Layout.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions/userActions';
+import { fetchTweets } from '../actions/tweetsActions';
+
+@connect((store) => {
+  
+  // mapStateToProps
+  return {
+    user: store.user.user,
+    userFetched: store.user.fetched,
+    tweets: store.tweets.tweets
+  }
+})
+export default class Layout extends React.Component {
+  componentWillMount() {
+    this.props.dispatch(fetchUser());
+  }
+  
+  fetchTweets(e){
+    e.preventDefault();
+    this.props.dispatch(fetchTweets());
+  }
+  
+  render() {
+    const { user, tweets } = this.props;
+    //console.log(this.props)
+    if(!tweets.length){
+      return <button onClick={this.fetchTweets.bind(this)}>load tweets</button>
+    }
+    
+    const mappedTweets = tweets.map(tweet => <li>{tweet.text}</li>);
+    
+    return <div>
+      <h1>{user.name}</h1>
+      <ul>{mappedTweets}</ul>
+      </div>
+  }
+}
